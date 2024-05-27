b80a3729614d5af60ecd51fe4a5a5117e87c767a
f-radosta <168348485+f-radosta@users.noreply.github.com>
2024-05-27 16:00:06 +0200

    Delete php directory

--- php/get_username.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-session_start();
-
-if (isset($_SESSION['user_name'])) {
-    echo htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8');
-} else {
-    echo '';
-}
